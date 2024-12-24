e0198779efede909f834a431d7b754637fdbc2ad
Dark-prince <barathraj0504@gmail.com>
2024-12-24 19:16:51 +0530

    Add basic functionalitie to the page

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("odin-recipes").onclick = function () {
+  window.open("https://dark-prince05.github.io/odin-recipes", "_blank");
+};
+
+document.getElementById("landing-page").onclick = function () {
+  window.open("https://dark-prince05.github.io/landing-page", "_blank");
+};
+
+document.getElementById("rps").onclick = function () {
+  window.open("https://dark-prince05.github.io/rock-paper-scissors", "_blank");
+};
+
+document.getElementById("etch-a-sketch").onclick = function () {
+  window.open("https://dark-prince05.github.io/etch-a-sketch", "_blank");
+};
+
+document.getElementById("calc").onclick = function () {
+  window.open("https://dark-prince05.github.io/calculator", "_blank");
+};
+
+document.getElementById("form").onclick = function () {
+  window.open("https://dark-prince05.github.io/sign-up-form", "_blank");
+};
